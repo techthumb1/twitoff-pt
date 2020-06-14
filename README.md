4067443c43dd08713eec85d5f53590d700a43128
techthumb1 <robinsonjason761@gmail.com>
2020-06-14 11:11:39 -0500

    Setup development database

--- README.md
+++ README.md
@@ -15,10 +15,16 @@ cd twitoff-pt5/
 pipenv install
 ```
 
+Migrate the database:
+
+'''sh
+FLASK_APP=web_app flask db init
+FLASK_APP=web_app flask db migrate
+FLASK_APP=web_app flask db upgrade
+'''
 
 # Usage
 
 ```sh
-FLASK_APP=hello.py flask run
 FLASK_APP=web_app flask run
 ```
--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,14 +1,23 @@
 from flask import Flask
 
+from web_app.models import db, migrate
 from web_app.routes.home_routes import home_routes
 from web_app.routes.book_routes import book_routes
-#from web_app.routes.book_routes import book_routes
+
+
+#DATABASE_URI = "sqlite:///C:r\\Users\\Aarons\\Desktop\\twitoff-pt5\\twitoff_development_pt5.db"
+DATABASE_URI = "sqlite:///C:\\Users\\Aarons\\Desktop\\TWITOFF-PT5\\web_app\\twitoff_development.db" 
 
 def create_app():
     app = Flask(__name__)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+    db.init_app(app)
+    migrate.init_app(app, db)
+
     app.register_blueprint(home_routes)
     app.register_blueprint(book_routes)
-    #app.register_blueprint(book_routes)
+
     return app
 
 if __name__ == "__main__":
